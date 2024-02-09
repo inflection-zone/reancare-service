970f493345dac6abb5a16e79c5a8662de48785a9
ram-inflection <ram.sarraf@sensesemi.com>
2024-02-09 16:56:54 +0530

    databases connected

--- src/rabbitmq/publisher.ts
+++ src/rabbitmq/publisher.ts
@@ -1,19 +1,19 @@
-import amqp from 'amqplib'
+// import amqp from 'amqplib'
 
-async function sendMessageToQueue(message) {
-    const connection = await amqp.connect('amqp://localhost');
-    const channel = await connection.createChannel();
+// async function sendMessageToQueue(message) {
+//     const connection = await amqp.connect('amqp://localhost');
+//     const channel = await connection.createChannel();
 
-    const queueName = 'tasksQueue'; // Specify the name of the queue
-    await channel.assertQueue(queueName, { durable: true });
+//     const queueName = 'tasksQueue'; // Specify the name of the queue
+//     await channel.assertQueue(queueName, { durable: true });
 
-    // Publish the message to the queue
-    channel.sendToQueue(queueName, Buffer.from(message), { persistent: true });
+//     // Publish the message to the queue
+//     channel.sendToQueue(queueName, Buffer.from(message), { persistent: true });
 
-    console.log(`Message '${message}' sent to queue`);
+//     console.log(`Message '${message}' sent to queue`);
 
-    await channel.close();
-    await connection.close();
-}
+//     await channel.close();
+//     await connection.close();
+// }
 
-sendMessageToQueue('Hello RabbitMQ!');
+// sendMessageToQueue('Hello RabbitMQ!');
