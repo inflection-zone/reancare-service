446625c9e028f3904d73f752b2d02bd186730dcf
ramsarraf <ram.sarraf@sensesemi.com>
2024-02-09 16:51:59 +0530

    databases connected

--- src.bg.worker/src.bg/config.ts
+++ src.bg.worker/src.bg/config.ts
@@ -1,8 +1,8 @@
-export default {
-    rabbitMQ: {
-      url: "amqp://localhost",
-      queues: {
-        messageQueue: "message_queue",
-      },
-    },
-  };
+// export default {
+//     rabbitMQ: {
+//       url: "amqp://localhost",
+//       queues: {
+//         messageQueue: "message_queue",
+//       },
+//     },
+//   };
